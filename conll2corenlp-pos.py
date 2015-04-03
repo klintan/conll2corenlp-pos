12d398a09334ab4bf641ed386812a0224c031241
Andreas Klintberg <ankl@kth.se>
2015-04-04 00:52:52 +0200

    added gitignore and fixed argument

--- conll2corenlp-pos.py
+++ conll2corenlp-pos.py
@@ -1,2 +1,19 @@
 import sys,os
+import fileinput
 
+f1 = open("output_training.txt", "a")
+sentence = []
+train_sentence = []
+doc = []
+
+with open (sys.argv[1], "r") as f:
+#with open ("talbanken-stanford-test.conll", "r") as f:
+    for line in f:
+        #print line
+        tokenized = line.split("\t")
+        print tokenized
+        doc.append(tokenized[1] + "\t" + tokenized[4] + "\n")
+        if len(doc) == 500:
+            f1.write("".join(doc))
+            doc = []
+f1.close()
